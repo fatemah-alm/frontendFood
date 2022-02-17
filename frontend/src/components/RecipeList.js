35827d5060f8260b5a62c4d8e1bc17d219c5e1ba
fatemah-alm <fatemah.alm@hotmail.com>
2022-02-17 20:50:53 +0400

    list

--- frontend/src/components/RecipeList.js
+++ frontend/src/components/RecipeList.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react";
+import recipeStore from "../stores/recipeStore";
+import Recipes from "./Recipes";
 
 const RecipeList = () => {
-  return <div>RecipeList</div>;
+  const recipeList = recipeStore.recipies.map((recipe) => {
+    return <Recipes recipe={recipe} key={recipe.id} />;
+  });
+  useEffect(() => {
+    recipeStore.fetchRecipies();
+  }, []);
+  return <div>{recipeList}</div>;
 };
 
 export default observer(RecipeList);
